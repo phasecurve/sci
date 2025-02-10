44af7987e45161b3b3090c5a79febc0300dbb6c1
Mark Gray <mark.gray@phasecurve.com>
2025-02-10 17:07:47 +0000

    fix: make build work, adding missing .sqlx

--- scripts/initialise_pg.sh
+++ scripts/initialise_pg.sh
@@ -25,9 +25,11 @@ DB_HOST="${POSTGRES_HOST:=localhost}"
 if [[ -z "${SKIP_DOCKER}" ]]
 then
   docker run \
+    --network=host \
     -e POSTGRES_USER=${DB_USER} -e POSTGRES_PASSWORD=${DB_PASSWORD} \-e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
-    -d postgres postgres -N 1000
+    -d postgres \
+    postgres -N 1000
 fi
 
 # wait for ps to come up
